c31f943685e3778a0aa3f2b452fc17c407b76ad5
gurudattsa <gurudatt.gd04@gmail.com>
2021-11-04 20:31:45 +0530

    test

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,6 +1,5 @@
 import * as importCwd from "import-cwd";
 import {MyWorld} from "../world/MyWorld";
-//import {Before} from 'cucumber';
 
 // @ts-ignore
 const {Before, After} = importCwd("@cucumber/cucumber");
